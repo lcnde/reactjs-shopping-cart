ead012e7ae9d21bbac32224751a7fb8e89c38671
lcnde <176673026+lcnde@users.noreply.github.com>
2021-11-11 06:47:50 +0100

    feature: added routes, shop and home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/App.scss';
 import Menu from '@material-ui/icons/Menu';
+import Home from './components/Home';
+import Shop from './components/Shop';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
-      <Menu style={{ color: 'red', fontSize: '40px' }} />
+      <h1>Put the navbar component here</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+  return(
+    <div className="shop-page">
+      <h1>Hello welcome to our shop</h1>
+    </div>
+  )
+}
+
+export default Shop;
