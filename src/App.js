32be146080a016265fe3e3ef2c7907f873d80918
lcnde <176673026+lcnde@users.noreply.github.com>
2021-11-11 06:21:26 +0100

    feature: added and styled material icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/App.scss';
+import Menu from '@material-ui/icons/Menu';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Menu style={{ color: 'red', fontSize: '40px' }} />
     </div>
   );
 }
