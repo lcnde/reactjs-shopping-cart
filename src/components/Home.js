8d0ffe5d12dd6ad5a37ac331df22a671e7e0d98d
lcnde <176673026+lcnde@users.noreply.github.com>
2021-11-16 07:59:04 +0100

    fix: state not updating on first click

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Home.scss';
 
 const Home = (props) => {
 
   const addToCart = (prod) => {
-    
-    //if checkout includes prod.id then alert()
+    //if checkout includes prod.id then alert()  
     props.setCheckout(prevState => [...prevState, {id: prod.id, url: prod.url, title: prod.title, price: prod.price}])
-    props.handleCart();
-
-    console.log(props.checkout)
+    
   }
+
+  
+  useEffect(() => {
+    props.handleCart();
+    console.log(props.checkout)    
+  }, [props, props.checkout]) //not sure why it had to write props twice
 
   return(
     <div className="home-page">
@@ -20,7 +23,7 @@ const Home = (props) => {
             <img src={prod.url} alt="product" />
             <span className="product-title">{prod.title}</span>
             <span className="product-price">{prod.price} $</span>
-            <button className="add-button" onClick={(prod) => addToCart(prod)}>Add to cart</button>
+            <button className="add-button" onClick={() => addToCart(prod)}>Add to cart</button>
           </div>
         )
       }
