9e59f8f940ea079b1e272ba1c5913b75817da31f
lcnde <176673026+lcnde@users.noreply.github.com>
2021-11-16 05:49:10 +0100

    feature: added uniqid for each item

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import './styles/App.scss';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Navbar from './components/Navbar';
+import uniqid from 'uniqid';
 
 function App() {
   const [cartNumber, setCartNumber] = useState(0);
   const [products, setProducts] = useState([
-    {url: 'https://m.media-amazon.com/images/I/813Uik1b-7L._AC_UL320_.jpg', title: 'The Lean Startup by Eric Ries', price: '5,64'},
-    {url: 'https://m.media-amazon.com/images/I/416ipvjdhjS._AC_UL320_.jpg', title: 'Housekeeper Client Data Log Book | Professional House Cleaning Client Tracking Book | Appointment Book with A thru Z Alpha Tabs to Record', price: '5,30'},
-    {url: 'https://m.media-amazon.com/images/I/81iUXe9ygkL._AC_UL320_.jpg', title: 'Elon Musk: How the Billionaire CEO of SpaceX and Tesla is Shaping our Future', price: '10,85'},
-    {url: 'https://m.media-amazon.com/images/I/71-X9PLxPKL._AC_UL320_.jpg', title: 'Elon Musk: The Life, Lessons & Rules For Success', price: '8,83'},
-    {url: 'https://m.media-amazon.com/images/I/71iEVsSP1GL._AC_UL320_.jpg', title: 'The Power of Habit: Why We Do What We Do, and How to Change', price: '17,67'},
-    {url: 'https://m.media-amazon.com/images/I/81PIXB1RBEL._AC_UL320_.jpg', title: 'Influence: The Psychology of Persuasion', price: '17,74'},
-    {url: 'https://m.media-amazon.com/images/I/71j5fJvL6IL._AC_UL320_.jpg', title: 'The Definitive Book of Body Language: How to read others attitudes by their gestures', price: '9,86'},
-    {url: 'https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL320_.jpg', title: 'Rich Dad Poor Dad: What The Rich Teach Their Kids about Money That the Poor and Middle Class Do Not', price: '9,72'}
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/813Uik1b-7L._AC_UL320_.jpg', title: 'The Lean Startup by Eric Ries', price: '5,64'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/416ipvjdhjS._AC_UL320_.jpg', title: 'Housekeeper Client Data Log Book | Professional House Cleaning Client Tracking Book | Appointment Book with A thru Z Alpha Tabs to Record', price: '5,30'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/81iUXe9ygkL._AC_UL320_.jpg', title: 'Elon Musk: How the Billionaire CEO of SpaceX and Tesla is Shaping our Future', price: '10,85'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/71-X9PLxPKL._AC_UL320_.jpg', title: 'Elon Musk: The Life, Lessons & Rules For Success', price: '8,83'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/71iEVsSP1GL._AC_UL320_.jpg', title: 'The Power of Habit: Why We Do What We Do, and How to Change', price: '17,67'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/81PIXB1RBEL._AC_UL320_.jpg', title: 'Influence: The Psychology of Persuasion', price: '17,74'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/71j5fJvL6IL._AC_UL320_.jpg', title: 'The Definitive Book of Body Language: How to read others attitudes by their gestures', price: '9,86'},
+    {id: uniqid(), url: 'https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL320_.jpg', title: 'Rich Dad Poor Dad: What The Rich Teach Their Kids about Money That the Poor and Middle Class Do Not', price: '9,72'}
   ]);
   const [checkout, setCheckout] = useState([]);
 
@@ -29,8 +30,17 @@ function App() {
           setCartNumber={setCartNumber}
         />
         <Routes>
-          <Route path="/" exact element={<Home products={products} />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/" 
+            exact element={<Home  products={products} 
+                                  checkout={checkout}
+                                  setCheckout={setCheckout}
+                            />} 
+          />
+          <Route  path="/shop" 
+                  element={<Shop 
+
+                          />} 
+          />
         </Routes>
       </BrowserRouter>
     </div>
